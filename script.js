1726c146117d85c7333a23d891153c103a207e4b
ricardopresto <ricardopresto5@gmail.com>
2019-03-24 15:53:03 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,60 @@
+let weatherData = {location: "", description:"", maxTemp: "", minTemp :""};
+const city = document.getElementById("city");
+const search = document.getElementById("search");
+const loc = document.getElementById("loc");
+const description = document.getElementById("description");
+const max_temp = document.getElementById("max_temp");
+const min_temp = document.getElementById("min_temp");
+let place;
+
+document.addEventListener("keydown", keyPress);
+search.addEventListener("click", getWeather);
+
+city.focus();
+
+function getData(place) {
+    fetch("http://api.openweathermap.org/data/2.5/weather?q=" + place + "&units=metric&APPID=b9a098030deae92fa48e1b91be240011", {mode:"cors"})
+    .then(function(response) {
+        return response.json()
+    })
+    .then(function(response) {
+        console.log(response)
+        weatherData.location = (response.name + ", " + response.sys.country)
+        weatherData.description = (jsUcfirst(response.weather[0].description))
+        weatherData.maxTemp = response.main.temp_max
+        weatherData.minTemp = response.main.temp_min
+        updateDisplay();
+    })
+    .catch(function() {
+        console.error("Data not found")
+        description.textContent = "Not found"
+        loc.textContent = ""
+        max_temp.textContent = ""
+        min_temp.textContent = ""
+    })
+
+}
+
+
+function getWeather() {
+    place = city.value;
+    getData(place);
+}
+
+function updateDisplay() {
+    loc.textContent = weatherData.location;
+    description.textContent = weatherData.description;
+    max_temp.textContent = "Max temp: " + weatherData.maxTemp + "°C";
+    min_temp.textContent = "Min temp: " + weatherData.minTemp + "°C";
+}
+
+function jsUcfirst(string) 
+{
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+function keyPress(event) {
+      if (event.key == "Enter") {
+        getWeather();
+      }
+    }
